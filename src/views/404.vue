5e8200b7251b6b8cb2875ea43a49cb66ed17412d
Marlex31 <alexandru.merila@gmail.com>
2021-05-22 11:18:31 +0300

    Added pulsating anim to home btn

--- src/views/404.vue
+++ src/views/404.vue
@@ -3,11 +3,11 @@
     <div class="box">
         <h1 class="msg">Aïe ! Cette page est introuvable.</h1>
         <br><br><br><br>
-        <router-link to="/" class="flex-container msg">
+        <router-link to="/" class="flex-container msg heartbeat">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="10%">
                 <path d="M 16 2.59375 L 15.28125 3.28125 L 2.28125 16.28125 L 3.71875 17.71875 L 5 16.4375 L 5 28 L 14 28 L 14 18 L 18 18 L 18 28 L 27 28 L 27 16.4375 L 28.28125 17.71875 L 29.71875 16.28125 L 16.71875 3.28125 Z M 16 5.4375 L 25 14.4375 L 25 26 L 20 26 L 20 16 L 12 16 L 12 26 L 7 26 L 7 14.4375 Z"/>
             </svg>
-            <p>
+            <p style="padding-left: 2.5%;">
                 Va à la page d'accueil
             </p>
         </router-link>
@@ -26,6 +26,35 @@ export default {
 </script>
 
 <style scoped>
+
+
+.heartbeat {
+	animation: heartbeat 1.7s ease-in-out infinite both;
+}
+@keyframes heartbeat {
+  from {
+    transform: scale(1);
+    transform-origin: center center;
+    animation-timing-function: ease-out;
+  }
+  10% {
+    transform: scale(0.91);
+    animation-timing-function: ease-in;
+  }
+  17% {
+    transform: scale(0.98);
+    animation-timing-function: ease-out;
+  }
+  33% {
+    transform: scale(0.87);
+    animation-timing-function: ease-in;
+  }
+  45% {
+    transform: scale(1);
+    animation-timing-function: ease-out;
+  }
+}
+
 
 .flex-container{
   display: flex;
